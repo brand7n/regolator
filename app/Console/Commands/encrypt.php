523d591a67c7a97525616eab0bf57f9f7263206b
Brandin L Claar <brandin@remodulate.com>
2024-04-09 12:57:33 -0400

    testing quick login method

---
+++ app/Console/Commands/encrypt.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Str;
+use App\Models\User;
+
+class encrypt extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:encrypt {email} {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $user = new User;
+        $user->name = $this->argument('name');
+        $user->email = $this->argument('email');
+        $user->password = $random = Str::random(40);
+        $user->save();
+        
+        $user_data = json_encode([
+            'id' => $user->id,
+            //'email' => $user->email,
+            'hash' => $user->password,
+            //'salt' => Str::random(5),
+        ]);
+        $this->info($user_data);
+        $this->info(Crypt::encryptString($user_data));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Crypt;
+use App\Models\User;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::middleware([
     'auth:sanctum',
@@ -15,3 +17,16 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('quicklogin/{key}', function($key) {
+    $user_data = json_decode(Crypt::decryptString($key), true);
+    // now find the user
+    $user = User::where('id', $user_data['id'])->first();
+    if ($user && $user->password === $user_data['hash']){
+        Auth::login($user); // login user automatically
+        return redirect('dashboard');
+    } else {
+        return redirect('/');
+    }
+});
+
