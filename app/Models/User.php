beadf95bed8f54543831ea5882fa83cb316a3774
Brandin L Claar <brandin@remodulate.com>
2024-05-07 11:27:42 -0400

    log user model activity

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,8 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class User extends Authenticatable
 {
@@ -17,6 +19,13 @@ class User extends Authenticatable
     use HasProfilePhoto;
     use Notifiable;
     //use TwoFactorAuthenticatable;
+    use LogsActivity;
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logAll();
+    }
 
     /**
      * The attributes that are mass assignable.
