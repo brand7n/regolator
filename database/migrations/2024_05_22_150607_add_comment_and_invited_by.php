ea6cd593092aee080a9e5849c154d6a473081ade
Brandin L Claar <brandin@remodulate.com>
2024-05-22 11:13:03 -0400

    add comment and invited by columns

---
+++ database/migrations/2024_05_22_150607_add_comment_and_invited_by.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('comment')->nullable();
+            $table->unsignedBigInteger('invited_by_id')->nullable();
+            $table->foreign('invited_by_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
