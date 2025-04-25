d9be112bd233d3975757a9448b05685fe3f234f4
Brandin L Claar <brandin@remodulate.com>
2025-04-25 11:40:17 -0400

    simplify order verification scheduling

--- routes/console.php
+++ routes/console.php
@@ -3,10 +3,20 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-use App\Jobs\VerifyTransactions;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::job(new VerifyTransactions())->everyFiveMinutes();
+Schedule::call(function () {
+    $orders = \App\Models\Order::whereNull('verified_at')->get();
+    foreach ($orders as $order) {
+        $order->verify();
+
+        // remove stale orders that will never be verified
+        $order->refresh();
+        if ($order->created_at->diffInHours() > 1.0) {
+            $order->delete();
+        }
+    }
+})->everyFiveMinutes();
