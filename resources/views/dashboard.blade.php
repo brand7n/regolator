66e74dbccec38ecc68d1403c850578640b5278ec
Brandin L Claar <brandin@remodulate.com>
2025-11-12 18:22:54 +0000

    holidaze stuff

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
     use Illuminate\Support\Facades\Auth;
 
     $now = Carbon::now('America/New_York');
-    $target = Carbon::createFromFormat('Y-m-d H:i:s', '2025-08-01 14:00:00', 'America/New_York');
+    $target = Carbon::createFromFormat('Y-m-d H:i:s', '2025-12-12 14:00:00', 'America/New_York');
     $totalSeconds = $now->diffInSeconds($target);
     $hoursRemaining = floor($totalSeconds / 3600);
     $minutesRemaining = floor(($totalSeconds % 3600) / 60);
@@ -11,54 +11,51 @@
 
 <x-app-layout>
     <x-rego-section>
-        <x-slot name="title">NVHHH #1900: NITTANY CALLING</x-slot>
+        <x-slot name="title">Circle & Socket&apos;s Festive Holidaze Vessel Weekend </x-slot>
         <x-slot name="content">
                     <div class="flex-col items-center">
-                    <img class="h-96" src="/nittanycalling.jpg" />
+                    <img class="h-96" src="/holidaze2025.jpg" />
             <p class="py-3 font-semibold text-gray-800 dark:text-gray-200">
-August 1, 2025 2pm - August 3, 2025 11:59am</p>
+December 12, 2025 2pm - December 14, 2025 11:59am</p>
 <div class="animate-bounce font-semibold text-gray-800 dark:text-gray-200">({{ $hoursRemaining }} hours and {{ $minutesRemaining }} minutes from now)</div>
             <p class="py-3 font-semibold text-gray-800 dark:text-gray-200">
 <b>PLEASE READ CAREFULLY AS SOME POLICIES HAVE CHANGED</b></p>
 
             <p class="py-3 font-semibold text-gray-800 dark:text-gray-200">
-Our Tyrant DADDY'S HOME...BITCH and NVHHH again bring you the East Coast’s premier hash camp-out, nestled between Sproul and Susquehannock State Forests on the mighty West Branch of the Susquehanna! This will be our 22nd year at the fabulous Loggia Giosue Carducci 146 Campground (aka The Sons).</p>
-
-            <p class="py-3 font-semibold text-gray-800 dark:text-gray-200">
-NOTE: Short Bus regos will be $60 CASH paid at the registration table. If you want to be part of the Short Bus crew (and miss the REAL SATURDAY WILDERNESS TRAIL HARED BY THE EX-TYRANTS OF NITTANY VALLEY) indicate your interest during registration, pay the regular rate, and you will be contacted with further details. Plaheeease be aware there is limited space and not everyone applying is guaranteed a seat on the Short Bus.</p>
-
-            <p class="py-3 font-semibold text-gray-800 dark:text-gray-200">
-WE LOVE DOGS BUT SADLY, NO DOGS ALLOWED AT THIS EVENT. WE CANNOT ACCOMMODATE LARGE POP-UP CAMPERS / TRAVEL TRAILERS / RVs ABOVE VAN-ISH SIZE. CAR CAMPING / TENT CAMPING IS PREFERRED. SCHEDULE OF EVENTS IS APPROXIMATE AND SUBJECT TO CHANGE.</p>
+Circle and Socket: we’re gonna do $35.69 — you get trail &amp; circle beer, a hashpitality-lite cabin, and the first 72 to sign up get a one-of-a-kind Holidaze vessel.</p>
 
             <p class="py-3 font-semibold text-gray-800 dark:text-gray-200">
 LOCATION:<br>
-Sons of Italy Campground<br>
-44 Sons Rd, Lock Haven, PA 17745</p>
+Parallel Loop Cottages<br>
+Allegany State Park, NY</p>
 
             <p class="py-3 font-semibold text-gray-800 dark:text-gray-200">
-DETAILS ON HIS GLORIOUS THEME: <a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://en.wikipedia.org/wiki/London_Calling">NITTANY CALLING 😱</a></p>
-
-            <p class="py-3 font-semibold text-gray-800 dark:text-gray-200">
-COST: $165/REGO (Pay below ⬇️⬇️⬇️ after accepting our waiver)</p>
+COST: $35.69/REGO (Pay below ⬇️⬇️⬇️ after accepting our waiver)</p>
     </div>
         </x-slot>
     </x-rego-section>
     <x-rego-section>
         <x-slot name="title">LOCATION</x-slot>
         <x-slot name="content">
-            <livewire:location-map event-id="1" />
+            <livewire:location-map event-id="2" />
         </x-slot>
+    </x-rego-section>
+    <x-rego-section>
+         <x-slot name="title">EVENT INFO</x-slot>
+         <x-slot name="content">
+            <livewire:event-info event-id="2"/>
+         </x-slot>
     </x-rego-section>
     <x-rego-section>
         <x-slot name="title">ACCEPT WAIVER/PAY UP</x-slot>
         <x-slot name="content">
-            <livewire:paypal event-id="1" />
+            <livewire:paypal event-id="2" />
         </x-slot>
     </x-rego-section>
     <x-rego-section>
         <x-slot name="title">OH LAWD THEY COMIN'</x-slot>
         <x-slot name="content">
-            <livewire:regos event-id="1" />
+            <livewire:regos event-id="2" />
         </x-slot>
     </x-rego-section>
 </x-app-layout>
--- resources/views/livewire/paypal.blade.php
+++ resources/views/livewire/paypal.blade.php
@@ -83,7 +83,7 @@
                 Rego paid for {{ $name }} at {{ $rego_paid_at->timezone('US/Eastern')->toDateTimeString() }}.
             </h2>
             <p class="py-3 font-semibold text-gray-800 dark:text-gray-200">
-                Verify your profile and edit rego preferences by clicking below. Hint: This is also how you get on short bus.
+                Verify your profile and edit rego preferences by clicking below.
             </p>
             <x-button class="py-3 items-center animate-bounce" wire:click="edit">Edit</x-button>
         @endif
@@ -108,7 +108,7 @@
                     Further, I agree to defend, indemnify and hold harmless the Nittany Valley Hash House Harriers, its sponsors, representatives, officers and management from any and all claims which may result from my participation in this event.</p>
 
                 <p class="py-3 font-semibold text-gray-800 dark:text-gray-200">
-                    I certify I have read this Waiver, I understand it, and I agree to its terms relating to the NVHHH #1900 event or activity hosted by the Nittany Valley Hash House Harriers.</p>
+                    I certify I have read this Waiver, I understand it, and I agree to its terms relating to the event or activity hosted Nittany Valley Hash House Harriers.</p>
                 <x-button class="py-3 items-center" wire:click="accept_terms">I Accept</x-button>
                 <x-button class="py-3 items-center bg-red-500 dark:bg-red-500" wire:click="decline">I Cannot Attend</x-button>
             @endif
